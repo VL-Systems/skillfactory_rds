90bda4cf3b5d079cc5972e0ad118cf2a3568bb4e
Stark <thenewagen7@yandex.ru>
2020-03-24 13:46:09 +0300

    Added main.py

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,47 @@
+import numpy as np
+def game_core_v1(number):
+    '''Просто угадываем на random ни как не используя информацию о больше или меньше.
+       Функция Принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict:
+            break
+    return (count)  # выход из цикла, если угадали
+def score_game(game_core_v1):
+    '''Запускаем игру 1000 раз, чтоб узнать как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core_v1(number))
+    score = int(np.mean(count_ls))
+    print("Ваш алгоритм угадывает число в среднем за ",score," попыток")
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его
+       в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    predict = np.random.randint(1,100)
+    while number != predict:
+        count+=1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return(count) # выход из цикла, если угадали
 
+def game_core_v3(number):
+    count=0
+    predict =50
+    while number!=predict:
+        count+=1
+        if number>predict:
+            predict += 5
+        elif number<predict:
+            predict -= 3
+    return(count)
+
+# запускаем
+score_game(game_core_v3)
